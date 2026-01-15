433d563239428fde2b2649ccb159ec1a99a7478a
vikusyischka <vikusyischka@gmail.com>
2026-01-15 11:48:51 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Вікторія Фенцик on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
